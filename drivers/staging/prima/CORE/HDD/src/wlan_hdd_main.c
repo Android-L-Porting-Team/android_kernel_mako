21573b2ab1eea4d5f2d868737f2655a8cb3e6283
Ben Pye <ben@curlybracket.co.uk>
2014-07-09 21:57:33 +0100

    Fix warning

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
@@ -9105,8 +9105,10 @@ err_bap_close:
 
 err_close_adapter:
    hdd_close_all_adapters( pHddCtx );
+#ifdef CONFIG_ENABLE_LINUX_REG
 err_unregister_wiphy:
    wiphy_unregister(wiphy) ;
+#endif
 err_vosstop:
    vos_stop(pVosContext);
 
