90122844b5b8236401095e3e2bf2ad7c0a99ad57
ion-storm <defconoii@gmail.com>
2014-07-14 00:44:34 -0400

    Revert "kexec: Implement kexec-hardboot"
    This reverts commit d3e46a9a366ed46b21e459853c00f0b385761176.
    
    Conflicts:
    	arch/arm/configs/flo_defconfig

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -35,12 +35,6 @@
 #include "msm_watchdog.h"
 #include "timer.h"
 
-#ifdef CONFIG_KEXEC_HARDBOOT
-#include <asm/kexec.h>
-#endif
-
-extern unsigned get_cable_status(void);
-
 #define WDT0_RST	0x38
 #define WDT0_EN		0x40
 #define WDT0_BARK_TIME	0x4C
@@ -140,20 +134,11 @@ EXPORT_SYMBOL(msm_set_restart_mode);
 
 static void __msm_power_off(int lower_pshold)
 {
-	int reset = 0;
-
 	printk(KERN_CRIT "Powering off the SoC\n");
 #ifdef CONFIG_MSM_DLOAD_MODE
 	set_dload_mode(0);
 #endif
-	if (machine_is_apq8064_flo() || machine_is_apq8064_deb()) {
-		if (get_cable_status()) {
-			printk(KERN_CRIT "Go to charger mode!");
-			reset = 1;
-			 __raw_writel(0x776655FE, restart_reason);
-		}
-	}
-	 pm8xxx_reset_pwr_off(reset);
+	pm8xxx_reset_pwr_off(0);
 
 	if (lower_pshold) {
 		__raw_writel(0, PSHOLD_CTL_SU);
@@ -249,7 +234,9 @@ void set_kernel_crash_magic_number(void)
 
 void msm_restart(char mode, const char *cmd)
 {
+
 #ifdef CONFIG_MSM_DLOAD_MODE
+
 	/* This looks like a normal reboot at this point. */
 	set_dload_mode(0);
 
@@ -279,8 +266,6 @@ void msm_restart(char mode, const char *cmd)
 			__raw_writel(0x77665500, restart_reason);
 		} else if (!strncmp(cmd, "recovery", 8)) {
 			__raw_writel(0x77665502, restart_reason);
-		} else if (!strcmp(cmd, "rtc")) {
-			__raw_writel(0x77665503, restart_reason);
 		} else if (!strncmp(cmd, "oem-", 4)) {
 			unsigned long code;
 			code = simple_strtoul(cmd + 4, NULL, 16) & 0xff;
@@ -295,12 +280,6 @@ void msm_restart(char mode, const char *cmd)
 	if (in_panic == 1)
 		set_kernel_crash_magic_number();
 reset:
-#else
-	if (in_panic == 1) {
-		__raw_writel(0x6d630100, restart_reason);
-		pr_notice("in panic\n");
-		mdelay(500);
-	}
 #endif /* CONFIG_LGE_CRASH_HANDLER */
 
 	__raw_writel(0, msm_tmr0_base + WDT0_EN);
@@ -343,14 +322,6 @@ static int __init msm_pmic_restart_init(void)
 
 late_initcall(msm_pmic_restart_init);
 
-#ifdef CONFIG_KEXEC_HARDBOOT
-static void msm_kexec_hardboot_hook(void)
-{
-	// Set PMIC to restart-on-poweroff
-	pm8xxx_reset_pwr_off(1);
-}
-#endif
-
 static int __init msm_restart_init(void)
 {
 #ifdef CONFIG_MSM_DLOAD_MODE
@@ -365,10 +336,6 @@ static int __init msm_restart_init(void)
 	msm_tmr0_base = msm_timer_get_timer0_base();
 	restart_reason = MSM_IMEM_BASE + RESTART_REASON_ADDR;
 	pm_power_off = msm_power_off;
-
-#ifdef CONFIG_KEXEC_HARDBOOT
-	kexec_hardboot_hook = msm_kexec_hardboot_hook;
-#endif
 
 	return 0;
 }
